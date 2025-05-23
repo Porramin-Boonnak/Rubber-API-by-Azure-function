e0a178397adabdce02c710c7b39208f490ea02ad
Porramin Boonnak <mosphonez3@gmail.com>
2025-05-24 02:11:57 +0700

    update 24/6/2025

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -35,6 +35,10 @@ months_in_thai = [
     "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"
 ]
 
+months_in_english = [
+    "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+]
+
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     """Each request is redirected to the WSGI handler.
     """
@@ -57,19 +61,37 @@ def insert_rubber():
         # แปลงเป็นชื่อวันและเดือนภาษาไทย
         day_in_thai = days_in_thai.get(day_of_week, "Unknown day")
         month_in_thai = months_in_thai[month - 1]  # เนื่องจากเดือนเริ่มจาก 0 ใน list
+        month_in_english = months_in_english[month-1]
 
         # เปลี่ยนปีให้เป็น พ.ศ.
         buddhist_year = year + 543
         retry_key = str(uuid.uuid4())
         userId=find["own"]
-        if data["sent"] == 2 :
-            replyuser(userId, f"{day_in_thai}ที่ {day_of_month} {month_in_thai} {buddhist_year}\nเวลา {hour}:{minute}:{second}\nค่า PH = {data['ph']}\nปริมาณยางในถัง = {data['volumnall']} ml", retry_key)
-        elif data["sent"] == 1 :
-            replyuser(userId, f"{day_in_thai}ที่ {day_of_month} {month_in_thai} {buddhist_year}\nเวลา {hour}:{minute}:{second}\nขณะนี้ PH = {data['ph']} ซึ่งต่ำกว่า จะทำการเติมแอมโมเนีย เมื่อกลับมาเป็นปรกติจะแจ้งให้ท่านทราบ", retry_key)
-        elif data["sent"] == 5 :
-            replyuser(userId, f"{day_in_thai}ที่ {day_of_month} {month_in_thai} {buddhist_year}\nเวลา {hour}:{minute}:{second}\nขณะนี้ PH กลับมาปกติแล้ว\nค่า PH = {data['ph']}\nปริมาณยางในถัง = {data['volumnall']} ml", retry_key)
-        elif data["sent"] == 3 :
-            replyuser(userId, f"{day_in_thai}ที่ {day_of_month} {month_in_thai} {buddhist_year}\nเวลา {hour}:{minute}:{second}\nขณะนี้ มีการเติมน้ำยางเข้ามา {data['volumn']} ml\nปริมาณยางในถัง = {data['volumnall']} ml \nเหลือ {data['amm']} ml", retry_key)
+        url = f'https://api.line.me/v2/bot/profile/{userId}'
+        headers = {
+                    "Authorization": "Bearer "+channel_access_token
+                }
+    
+        response = requests.get(url, headers=headers)
+        language = response.json()
+        if language["language"] == "th" :
+            if data["sent"] == 2 :
+                replyuser(userId, f"{day_in_thai}ที่ {day_of_month} {month_in_thai} {buddhist_year}\nเวลา {hour}:{minute}:{second}\nค่า PH = {data['ph']}\nปริมาณยางในถัง = {data['volumnall']} ml", retry_key)
+            elif data["sent"] == 1 :
+                replyuser(userId, f"{day_in_thai}ที่ {day_of_month} {month_in_thai} {buddhist_year}\nเวลา {hour}:{minute}:{second}\nขณะนี้ PH = {data['ph']} ซึ่งต่ำกว่า จะทำการเติมแอมโมเนีย เมื่อกลับมาเป็นปรกติจะแจ้งให้ท่านทราบ", retry_key)
+            elif data["sent"] == 5 :
+                replyuser(userId, f"{day_in_thai}ที่ {day_of_month} {month_in_thai} {buddhist_year}\nเวลา {hour}:{minute}:{second}\nขณะนี้ PH กลับมาปกติแล้ว\nค่า PH = {data['ph']}\nปริมาณยางในถัง = {data['volumnall']} ml", retry_key)
+            elif data["sent"] == 3 :
+                replyuser(userId, f"{day_in_thai}ที่ {day_of_month} {month_in_thai} {buddhist_year}\nเวลา {hour}:{minute}:{second}\nขณะนี้ มีการเติมน้ำยางเข้ามา {data['volumn']} ml\nปริมาณยางในถัง = {data['volumnall']} ml \nเหลือ {data['amm']} ml", retry_key)
+        else:
+            if data["sent"] == 2 :
+                replyuser(userId, f"{day_of_week} {day_of_month} {month_in_english} {year}\n{hour}:{minute}:{second}\nPH = {data['ph']}\nvolumn = {data['volumnall']} ml", retry_key)
+            elif data["sent"] == 1 :
+                replyuser(userId, f"{day_of_week} {day_of_month} {month_in_english} {year}\n{hour}:{minute}:{second}\nNow PH = {data['ph']} Ammonia will be refilled once the level falls below the acceptable range. You will be informed once the condition returns to normal", retry_key)
+            elif data["sent"] == 5 :
+                replyuser(userId, f"{day_of_week} {day_of_month} {month_in_english} {year}\n{hour}:{minute}:{second}\nThe pH level has now returned to normal.\nPH = {data['ph']}\nvolumn = {data['volumnall']} ml", retry_key)
+            elif data["sent"] == 3 :
+                replyuser(userId, f"{day_of_week} {day_of_month} {month_in_english} {year}\n{hour}:{minute}:{second}\nThe latex is currently being added. {data['volumn']} ml\nvolumn = {data['volumnall']} ml \nAmmonia remaining {data['amm']} ml", retry_key)
         rubber.insert_one(data)
     return jsonify("update complate")
 
@@ -77,6 +99,13 @@ def insert_rubber():
 @app.route("/data/<string:userid>", methods=['GET'])
 def data(userid):
     find = machine.find_one({"own": str(userid)})
+    url = f'https://api.line.me/v2/bot/profile/{userid}'
+    headers = {
+                "Authorization": "Bearer "+channel_access_token
+            }
+
+    response = requests.get(url, headers=headers)
+    language = response.json()
     if find :
         alldata = list(rubber.find({"machineID":str(find["_id"])}))
         for doc in alldata:
@@ -85,7 +114,10 @@ def data(userid):
         return render_template("data.html", content=alldata )
     else :
         retry_key = str(uuid.uuid4())
-        replyuser(userid,"ไม่พบข้อมูล",retry_key)
+        if language["language"] == "th" :
+            replyuser(userid,"ไม่พบข้อมูล",retry_key)
+        else:
+            replyuser(userid,"Data not found.",retry_key)
 
 
 @app.route("/callback", methods=['POST'])
@@ -95,14 +127,27 @@ def callback():
         text = req["events"][0]["message"]["text"].split()
         userId = req["events"][0]["source"]["userId"]
         reply_token = req['events'][0]['replyToken']
+        url = f'https://api.line.me/v2/bot/profile/{userId}'
+        headers = {
+                    "Authorization": "Bearer "+channel_access_token
+                }
+
+        response = requests.get(url, headers=headers)
+        language = response.json()
         if text[0] == "Data":
             find = machine.find_one({"own": str(userId)})
             if find:
                 reply(reply_token, f"https://web-hook-001.azurewebsites.net/data/{userId}")
             else:
-                reply(reply_token,"ไม่พบเครื่องที่ลงทะเบียน")
+                if language["language"] == "th" :
+                    reply(reply_token,"ไม่พบเครื่องที่ลงทะเบียน")
+                else:
+                    reply(reply_token,"No registered device found.")
         elif text[0] == "Register":
-            reply(reply_token,f"กรุณาพิมพ์ข้อความตามตัวอย่างนี้\nRegisterID xxxxxxx")
+            if language["language"] == "th" :
+                reply(reply_token,f"กรุณาพิมพ์ข้อความตามตัวอย่างนี้\nRegisterID xxxxxxx")
+            else :
+                reply(reply_token,f"Please type the message according to this example.\nRegisterID xxxxxxx")
         elif text[0] == "RegisterID":
             data = machine.find_one({"_id": str(text[1]), "own": {"$exists": False}})
             if data:
@@ -110,11 +155,20 @@ def callback():
                     { "_id" : str(text[1]) },
                     { "$set": { "own" : userId } }
                 )
-                reply(reply_token,"ลงทะเบียนเรียบร้อย")
+                if language["language"] == "th" :
+                    reply(reply_token,"ลงทะเบียนเรียบร้อย")
+                else :
+                    reply(reply_token,"Registration completed.")
             else:
-                reply(reply_token,"ไม่พบเครื่อง หรือ เครื่องนี้ลงทะเบียนไปแล้ว")
+                if language["language"] == "th" :
+                    reply(reply_token,"ไม่พบเครื่อง หรือ เครื่องนี้ลงทะเบียนไปแล้ว")
+                else:
+                    reply(reply_token,"Device not found or already registered.")
         elif text[0] == "Unregister":
-            reply(reply_token,"กรุณาพิมพ์ข้อความตามตัวอย่างนี้\nUnregisterID xxxxxxx")
+            if language["language"] == "th" :
+                reply(reply_token,"กรุณาพิมพ์ข้อความตามตัวอย่างนี้\nUnregisterID xxxxxxx")
+            else :
+                reply(reply_token,"Please type the message according to this example.\nUnregisterID xxxxxxx")
         elif text[0] == "UnregisterID":
             data = machine.find_one({"_id": str(text[1]),"own":str(userId)})
             if data:
@@ -122,11 +176,20 @@ def callback():
                     { "_id" : str(text[1]) },
                     { "$unset": { "own":str(userId) } }
                 )
-                reply(reply_token,"ยกเลิกทะเบียนเรียบร้อย")
+                if language["language"] == "th" :
+                    reply(reply_token,"ยกเลิกทะเบียนเรียบร้อย")
+                else:
+                    reply(reply_token,"Unregistration completed.")
             else:
-                reply(reply_token,"ไม่พบเครื่อง")
+                if language["language"] == "th" :
+                    reply(reply_token,"ไม่พบเครื่อง")
+                else :
+                    reply(reply_token,"Device not found")
         else:
-            reply(reply_token,"เลือกเมนูด้านล่างเพื่อทำรายการ") 
+            if language["language"] == "th" :
+                reply(reply_token,"เลือกเมนูด้านล่างเพื่อทำรายการ") 
+            else:
+                reply(reply_token,"Please select a menu below to proceed.") 
         
         return 'OK', 200
     except Exception as e:
